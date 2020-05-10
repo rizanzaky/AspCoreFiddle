8fc608b5a441e16dc566b031dd0f56539a45c2aa
Rizan Zaky <rizan149@gmail.com>
2020-05-10 19:34:16 +0530

    get message from settings

--- CafeFurs/CafeFurs.Web/Pages/Restaurants/List.cshtml.cs
+++ CafeFurs/CafeFurs.Web/Pages/Restaurants/List.cshtml.cs
@@ -6,26 +6,29 @@ using CafeFurz.Core.Models;
 using CafeFurz.Core.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace CafeFurs.Web.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
+        private readonly IConfiguration _configuration;
 
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
 
-        public ListModel(IRestaurantData restaurantData)
+        public ListModel(IRestaurantData restaurantData, IConfiguration configuration)
         {
             this.restaurantData = restaurantData;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-            Message = "Hello World";
+            Message = _configuration["Application"];
             Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
