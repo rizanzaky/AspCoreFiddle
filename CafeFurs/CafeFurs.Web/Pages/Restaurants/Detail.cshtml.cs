344a0528df0e49201ee74fd4879096908f688b68
Rizan Zaky <rizan149@gmail.com>
2020-05-10 17:08:34 +0530

    add restaurant details and font awesome with libMan

---
+++ CafeFurs/CafeFurs.Web/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CafeFurz.Core.Models;
+using CafeFurz.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CafeFurs.Web.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public Restaurant Restaurant { get; set; }
+
+        public DetailModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public void OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetRestaurantById(restaurantId);
+        }
+    }
+}
